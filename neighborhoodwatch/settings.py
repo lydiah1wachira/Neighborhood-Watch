f5a570eef2601eab00b1d77ac0f4caeed814aa06
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-18 14:38:10 +0300

    create gitignore file and place the virtual folder inside it.

--- neighborhoodwatch/settings.py
+++ neighborhoodwatch/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'neighborhood'
+    'neighborhood',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
