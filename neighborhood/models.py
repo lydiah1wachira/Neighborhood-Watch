9c22d1a13f87eb00ef0d5decff8f242fc5da111d
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-20 13:35:17 +0300

    create a profile model.

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -37,3 +37,11 @@ class Neighbourhood(models.Model):
     def __str__(self):
         return f'{self.name}'
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(max_length=254, blank=True)
+    image =CloudinaryField(default='default.jpg')
+    hood = models.ForeignKey(Neighbourhood, on_delete=models.SET_NULL, null=True, related_name='users', blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username} profile'
