671c9b4a0a5f38031dc08e749e2eb65e9b8e34b2
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-20 13:00:06 +0300

    make migrations to connect to the postgres database.

--- neighborhoodwatch/settings.py
+++ neighborhoodwatch/settings.py
@@ -88,8 +88,10 @@ WSGI_APPLICATION = 'neighborhoodwatch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'hood',
+        'USER': 'wachira',
+        'PASSWORD': 'Lydiah007',
     }
 }
 
@@ -118,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
